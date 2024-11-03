46ea08c9edb20ccd513f71604da5e893c3792ab7
Aralines <kirillkirill9@yandex.ru>
2024-11-03 13:02:33 +0300

    Основная сборка
    Основная сборка

--- life of a guard/Assets/Scripts/Tower/teleport.cs
+++ life of a guard/Assets/Scripts/Tower/teleport.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class teleport : MonoBehaviour
@@ -5,6 +6,7 @@ public class teleport : MonoBehaviour
     [SerializeField] private Transform newPosition;
     [SerializeField] private gameInput input;
     private const string PLAYERTAG = "Player";
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag(PLAYERTAG) &&
