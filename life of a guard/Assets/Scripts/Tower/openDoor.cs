0994297247aef29bf5fd1acf76c00f7000fdc406
Aralines <kirillkirill9@yandex.ru>
2024-11-03 13:08:57 +0300

    Это основная

---
+++ life of a guard/Assets/Scripts/Tower/openDoor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    private Animator _animator;
+    private const string EXITCONTROL = "ExitControl";
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _animator.SetBool(EXITCONTROL, true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _animator.SetBool(EXITCONTROL, false);
+    }
+    
+}
