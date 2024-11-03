41eb63eeb1bfe55f9fee6215c450cc225e656ec0
Elias <eliaseromanov@gmail.com>
2024-11-04 01:06:22 +0700

    Pause Menu

---
+++ life of a guard/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private gameInput input;
+    [SerializeField] private GameObject pauseMenu;
+
+    public void PauseMenu()
+    {
+        pauseControl(true);
+    }
+
+    public void Resume()
+    {
+        pauseControl(false);
+    }
+    
+    private void pauseControl(bool pause)
+    {
+        Time.timeScale = pause ? 0 : 1;
+        pauseMenu.SetActive(pause);
+    }
+}
