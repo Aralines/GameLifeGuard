69129995809c3b347268a0a6ae86b806f3801c36
Elias <eliaseromanov@gmail.com>
2024-11-04 18:49:26 +0700

    Enemy

--- life of a guard/Assets/Scripts/Enemys/EnemyTarget.cs
+++ life of a guard/Assets/Scripts/Enemys/EnemyTarget.cs
@@ -5,16 +5,29 @@ public class EnemyTarget : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] private float enemySpeed = 2f;
+    private SpriteRenderer spriteRenderer;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
     private void FixedUpdate()
     {
         moveEnemy();
-        
     }
 
     private void moveEnemy()
     {
         Vector3 direction = target.transform.position - transform.position;
+        if (target.position.x < transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
         transform.position += direction * enemySpeed * Time.deltaTime;
     }
 }
